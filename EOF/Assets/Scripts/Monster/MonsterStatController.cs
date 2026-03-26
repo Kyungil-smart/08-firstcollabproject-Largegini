a08e53f022662efd64e6ce32e60d3de93c590535
156Sheong <waladeus@gmail.com>
2026-03-26 20:06:37 +0900

    몬스터 스텟 컨트롤러의 test 네임스페이스 제거
    테스트 용도가 아니라 실제 사용할 스크립트라서 네임스페이스 제거

--- EOF/Assets/Scripts/Monster/MonsterStatController.cs
+++ EOF/Assets/Scripts/Monster/MonsterStatController.cs
@@ -4,79 +4,77 @@ using UnityEngine;
 
 // 작성자 : 한성우
 // 잘 되는지 테스트용 단 한 마리의 몬스터 컨트롤러(추후 삭제해야함)
-namespace Test
+
+// [ExecuteInEditMode]
+public class MonsterStatController : MonoBehaviour
 {
-    // [ExecuteInEditMode]
-    public class MonsterStatController : MonoBehaviour
+    [Header("입력이 필요한 값")]
+    [SerializeField] private int thisID;
+
+    /*
+    [Header("자동으로 불러들이는 값 예시")]
+    [SerializeField] private string thisLocalizeID;
+    [SerializeField] private int thisMaxHP;
+    [SerializeField] private int thisDamage;
+    */
+
+    // 테이블 기반 클래스와 몬스터 상속 클래스 가져오기
+    MonsterTable table;
+    Monster thisScript;
+
+    private bool isInitialized = false;
+
+
+
+
+    private void Awake()
     {
-        [Header("입력이 필요한 값")]
-        [SerializeField] private int thisID;
+        // if (DataManager._instance.GetMonsterTable() != null) table = DataManager._instance.GetMonsterTable();
+        InitializeMonster(thisID);
+    }
 
+
+    private void Update()
+    {
         /*
-        [Header("자동으로 불러들이는 값 예시")]
-        [SerializeField] private string thisLocalizeID;
-        [SerializeField] private int thisMaxHP;
-        [SerializeField] private int thisDamage;
-        */
-
-        // 테이블 기반 클래스와 몬스터 상속 클래스 가져오기
-        MonsterTable table;
-        Monster thisScript;
-
-        private bool isInitialized = false;
-
-        
-
-
-        private void Awake()
+        if (!isInitialized)
         {
-            // if (DataManager._instance.GetMonsterTable() != null) table = DataManager._instance.GetMonsterTable();
             InitializeMonster(thisID);
         }
+        */
+    }
+
+    public void InitializeMonster(int id)
+    {
+
+        if (DataManager._instance == null) return;
 
 
-        private void Update()
+        table = DataManager._instance.GetMonsterTable();
+        thisScript = GetComponent<Monster>();   // 몬스터나 몬스터 상속받는 스크립트 찾기
+
+
+
+        // 테이블이 없거나 id 없으면 예외 처리
+        if (table == null || !table.MonsterDic.ContainsKey(thisID)) Debug.LogError($"ID {thisID} 몬스터 확인 불가!");
+
+        // 
+
+
+        else
         {
-            /*
-            if (!isInitialized)
-            {
-                InitializeMonster(thisID);
-            }
-            */
+            MonsterData myData = table.MonsterDic[thisID];
+
+            // thisScript. = myData.LocalizeID;
+            thisScript._tableMaxHP = myData.HP;
+            thisScript._minDamage = myData.Damage_1;
+
+            thisScript.InitStat();
         }
 
-        public void InitializeMonster(int id)
-        {
-
-            if (DataManager._instance == null) return;
-
-
-            table = DataManager._instance.GetMonsterTable();
-            thisScript = GetComponent<Monster>();   // 몬스터나 몬스터 상속받는 스크립트 찾기
 
 
 
-            // 테이블이 없거나 id 없으면 예외 처리
-            if (table == null || !table.MonsterDic.ContainsKey(thisID)) Debug.LogError($"ID {thisID} 몬스터 확인 불가!");
-
-            // 
-
-
-            else
-            {
-                MonsterData myData = table.MonsterDic[thisID];
-
-                // thisScript. = myData.LocalizeID;
-                thisScript._tableMaxHP = myData.HP;
-                thisScript._minDamage = myData.Damage_1;
-
-                thisScript.InitStat();
-            }
-
-
-
-
-            isInitialized = true;
-        }
+        isInitialized = true;
     }
 }
