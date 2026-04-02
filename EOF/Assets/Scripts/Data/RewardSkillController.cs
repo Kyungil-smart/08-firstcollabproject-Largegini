2020c5643685e1843f8f7892990221b07dd52b1f
156Sheong <waladeus@gmail.com>
2026-04-03 01:23:54 +0900

    RewardSkillController 기능 추가 및 연결

---
+++ EOF/Assets/Scripts/Data/RewardSkillController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class RewardSkillController : MonoBehaviour
+{
+    public void UseSkillChain01()
+    {
+        // 기능 추가 필요
+    }
+
+
+    public void UseSkillChain02()
+    {
+        // 기능 추가 필요
+    }
+
+
+    public void UseRejuvenate()
+    {
+        // 기능 추가 필요
+    }
+
+
+    public void UseBulwark()
+    {
+        // 기능 추가 필요
+    }
+
+
+    public void UseOnslaught01()
+    {
+        // 기능 추가 필요
+    }
+
+
+    public void UseOnslaught02()
+    {
+        // 기능 추가 필요
+    }
+
+
+    public void UseResurrection()
+    {
+        // 기능 추가 필요
+    }
+
+
+}
